1293d4642a334b37f1543cf9a999f9824a364d7f
Kam1Cadzee <39504960+Kam1Cadzee@users.noreply.github.com>
2019-07-11 01:14:36 +0300

    add component "BoxShadow"

---
+++ src/components/Shared/BoxShadow/BoxShadow.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './BoxShadow.module.css';
+
+const BoxShadow = ({ children, className, boxShadow }) => {
+  const shadowName = `shadowDepth${boxShadow}`;
+  return <div className={`${className} ${css[shadowName]}`}>{children}</div>;
+};
+BoxShadow.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.object])
+    .isRequired,
+  boxShadow: PropTypes.number,
+  className: PropTypes.string,
+};
+BoxShadow.defaultProps = {
+  boxShadow: 1,
+  className: '',
+};
+
+export default BoxShadow;
