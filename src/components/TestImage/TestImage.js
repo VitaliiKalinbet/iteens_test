f80662d50aea5c1b1841920171ab88a0f127f129
Kam1Cadzee <39504960+Kam1Cadzee@users.noreply.github.com>
2019-07-17 22:53:14 +0300

    add parser for c#, js, java

--- src/components/TestImage/TestImage.js
+++ src/components/TestImage/TestImage.js
@@ -2,9 +2,20 @@ import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { monokaiSublime } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import PropTypes from 'prop-types';
+import csh from 'prettier-csharp';
+import prettier from 'prettier/standalone';
+import babel from 'prettier/parser-babylon';
+import java from 'prettier-plugin-java';
 import BoxShadow from '../Shared/BoxShadow/BoxShadow';
 
 const TestImage = ({ language, codeString }) => {
+  const code = prettier.format(codeString, {
+    semi: true,
+    parser: language,
+    plugins: [csh, java, babel],
+    tabWidth: 2,
+  });
+
   return (
     <BoxShadow boxShadow={2}>
       <SyntaxHighlighter
@@ -12,13 +23,14 @@ const TestImage = ({ language, codeString }) => {
         style={monokaiSublime}
         showLineNumbers
       >
-        {codeString}
+        {code}
       </SyntaxHighlighter>
     </BoxShadow>
   );
 };
 TestImage.propTypes = {
-  language: PropTypes.oneOf(['csharp', 'javascript', 'java']).isRequired,
+  language: PropTypes.oneOf(['cs', 'java', 'babel']).isRequired,
   codeString: PropTypes.string.isRequired,
 };
+
 export default TestImage;
--- src/pages/ResultPage/ResultPage.js
+++ src/pages/ResultPage/ResultPage.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import TestImage from '../../components/TestImage/TestImage';
+
+const code =
+  'public class Fibonacci { public static void main(String[] args) { int n = 10, t1 = 0, t2 = 1; System.out.print("First " + n + " terms: "); for (int i = 1; i <= n; ++i) { System.out.print(t1 + " + "); int sum = t1 + t2; t1 = t2; t2 = sum; } } }';
 
 class ResultPage extends Component {
   state = {};
@@ -7,6 +11,10 @@ class ResultPage extends Component {
     return (
       <div>
         <h2>ResultPage</h2>
+
+        {/* codeString - кусок кода */}
+        {/* language - тип языка(cs - C#, babel - javascript, java - java */}
+        <TestImage codeString={code} language="java" />
       </div>
     );
   }
