a48265f098d0c318336e7023b141ebba0e1facdb
lapeev16@gmail.com <lapeev16@gmail.com>
2019-07-18 21:08:28 +0300

    init startPageInit

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import startPageReducer from './startPageRedux/startPageReducers';
+import startPageSetLanguage from './startPageRedux/startPageSetLanguage';
 // тут импортируем редюсеры
 
 const rootReducer = combineReducers({
+  languagesInfoArr: startPageReducer,
+  currentLanguageId: startPageSetLanguage,
   // тут подключаем редюсеры
 });
 
